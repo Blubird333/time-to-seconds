db90e3aaa3e325bb8eab29da48f1b65e5b804b7b
C Raj Dheeraj Reddy <73807445+Blubird333@users.noreply.github.com>
2022-06-22 12:18:34 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include "time.cpp"
+using namespace std;
+
+void main(void){
+    int a,b,c;
+    string A,B,C;
+    cout<<"Hours?";
+    cin>>a;
+    cout<<"Minutes?";
+    cin>>b;
+    cout<<"Seconds?";
+    
+    if(a<10){
+        A="0"+ to_string(a);
+    }else{
+        A=to_string(a);
+    }
+    if(b<10){
+        B="0"+ to_string(b);
+    }else{
+        B=to_string(b);
+    }
+    if(c<10){
+        C="0"+ to_string(c);
+    }else{
+        C=to_string(c);
+    }
+    
+    cout<<"The time is = "<<A<<":"<<B<<":"<<C<<endl;
+    
+    time1 time;
+    int total_time_in_sec = time1.totalsec(a,b,c);
+    
+    cout<<"Time in total seconds: "<<total_time_in_sec;
+    
+    
+}
